d12b2c8c2fe4272e8228b154515b4197922ae22c
arking115 <rusalexandru115@gmail.com>
2024-04-03 09:22:37 +0300

    set up

---
+++ src/main/java/com/example/poftabuna/PoftaBunaApplication.java
@@ -0,0 +1,13 @@
+package com.example.poftabuna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoftaBunaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoftaBunaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/poftabuna/PoftaBunaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.poftabuna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoftaBunaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
